7e6d0cd1d6ded38081f533041c61bc3df480ce7c
MiaoLala <111435584+MiaoLala@users.noreply.github.com>
2025-06-24 07:36:02 +0800

    app.py 업데이트

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 
 from user_registration import register_user  # 員編登記
 from meeting_notify import send_meeting_notification  # 會議通知
+from meeting_notification import send_meeting_notification
 
 app = Flask(__name__)
 
